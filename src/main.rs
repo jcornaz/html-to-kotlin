84ae2165d73751207f005cd5602c145248237c22
Jonathan Cornaz <jonathan.cornaz@gmail.com>
2023-09-21 13:00:01 +0200

    first draft

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,128 @@
-fn main() {}
+use std::{
+    borrow::Cow,
+    fmt,
+    fmt::{Display, Formatter},
+    io,
+    io::Read,
+};
+
+use main_error::MainError;
+use tl::{Node, Parser, ParserOptions};
+
+fn main() -> Result<(), MainError> {
+    let mut raw = String::new();
+    io::stdin().read_to_string(&mut raw)?;
+    let dom = tl::parse(&raw, ParserOptions::default())?;
+    for node in dom.children() {
+        let node = build_kotlin_node(node.get(dom.parser()).unwrap(), dom.parser());
+        let dsl_node = KotlinDslIndentedNode {
+            indent: 0,
+            node: &node,
+        };
+        println!("{dsl_node}");
+    }
+    Ok(())
+}
+
+fn build_kotlin_node<'a>(node: &'a Node, parser: &'a Parser) -> KotlinDslNode {
+    match node {
+        Node::Tag(tag) => {
+            let (classes, attributes): (
+                Vec<(Cow<'a, str>, Cow<'a, str>)>,
+                Vec<(Cow<'a, str>, Cow<'a, str>)>,
+            ) = tag
+                .attributes()
+                .iter()
+                .filter_map(|(key, value)| Some((key, value?)))
+                .partition(|(key, _)| key.as_ref() == "class");
+            let children = tag
+                .children()
+                .top()
+                .iter()
+                .filter_map(|child| Some(build_kotlin_node(child.get(parser)?, parser)))
+                .collect();
+            KotlinDslNode::Tag {
+                name: tag.name().as_utf8_str().as_ref().to_owned(),
+                classes: classes
+                    .into_iter()
+                    .next()
+                    .map(|(_, v)| v.as_ref().to_owned()),
+                attributes: attributes
+                    .into_iter()
+                    .map(|(k, v)| (k.as_ref().to_owned(), v.as_ref().to_owned()))
+                    .collect(),
+                children,
+            }
+        }
+        Node::Raw(raw) => KotlinDslNode::String(raw.as_utf8_str().as_ref().to_owned()),
+        Node::Comment(comment) => KotlinDslNode::Comment(comment.as_utf8_str().as_ref().to_owned()),
+    }
+}
+
+struct KotlinDslIndentedNode<'a> {
+    indent: u16,
+    node: &'a KotlinDslNode,
+}
+
+enum KotlinDslNode {
+    Tag {
+        name: String,
+        classes: Option<String>,
+        attributes: Vec<(String, String)>,
+        children: Vec<KotlinDslNode>,
+    },
+    String(String),
+    Comment(String),
+}
+
+impl<'a> Display for KotlinDslIndentedNode<'a> {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        write_indent(f, self.indent)?;
+        match &self.node {
+            KotlinDslNode::Tag {
+                name,
+                classes,
+                attributes,
+                children,
+            } => {
+                write!(f, "{name}")?;
+                if let Some(classes) = classes {
+                    write!(f, "(classes=\"{classes}\"")?;
+                }
+                if attributes.is_empty() && children.is_empty() {
+                    if classes.is_none() {
+                        write!(f, "()")?;
+                    }
+                    {
+                        write!(f, "")?;
+                    }
+                    return Ok(());
+                }
+                writeln!(f, " {{")?;
+                for (key, value) in attributes.iter() {
+                    write_indent(f, self.indent + 1)?;
+                    writeln!(f, "{key}=\"{value}\"")?;
+                }
+                for child in children.iter().map(|node| KotlinDslIndentedNode {
+                    indent: self.indent + 1,
+                    node,
+                }) {
+                    write_indent(f, self.indent + 1)?;
+                    writeln!(f, "{child}")?;
+                }
+                write_indent(f, self.indent)?;
+                write!(f, "}}")?;
+                Ok(())
+            }
+            KotlinDslNode::String(s) => write!(f, "+\"{s}\""),
+            KotlinDslNode::Comment(s) => write!(f, "// {s}"),
+        }
+    }
+}
+
+fn write_indent(buffer: &mut impl fmt::Write, indent_level: u16) -> Result<(), fmt::Error> {
+    for _ in 0..indent_level {
+        write!(buffer, " ")?;
+    }
+    Ok(())
+}
